4bf78bfa7c15c9631c9831a12d529e518a3a1ba2
vyachnd <vyacheslav.nodata@yandex.ru>
2022-07-27 20:26:49 +0300

    feat: add calculator init

--- calculator/index.js
+++ calculator/index.js
@@ -1,1 +1,9 @@
-console.log('index.js loaded');
+import Calculator from './components/calculator/index.js';
+
+const appContent = document.querySelector('.app__content');
+
+if (appContent instanceof HTMLElement) {
+  (() => new Calculator({
+    node: appContent,
+  }))();
+}
